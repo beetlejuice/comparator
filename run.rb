2799249541ab38e7e9d3c40c6eaa6852fff41680
andrew.rymar <andrew.rymar@gmail.com>
2015-05-12 21:55:08 +0300

    Change configuration to command line arguments

--- run.rb
+++ run.rb
@@ -1,7 +1,13 @@
 require 'yaml'
 require_relative 'comparator'
 
-config = YAML.load(File.open 'config.yml')
+# config = YAML.load(File.open 'config.yml')
+config = {
+  'test_db'      => ARGV[0],
+  'reference_db' => ARGV[1],
+  'table'        => ARGV[2],
+  'compare_key'  => ARGV[3]
+}
 
 cmp = Comparator.new config
 result = cmp.run
